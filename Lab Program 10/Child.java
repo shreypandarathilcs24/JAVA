14e309d05300a17e8264292d9820209fe0e05434
shreypandarathilcs24 <shreypandarathil.cs24@bmsce.ac.in>
2025-12-09 12:07:57 +0530

    Delete Lab Program 10 directory

--- Lab Program 10/Child.java
+++
@@ -1,9 +0,0 @@
-import java.io.*;
-
-public class Child {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String message = br.readLine();   // Receive message from parent
-        System.out.println("Child received: " + message);
-    }
-}
